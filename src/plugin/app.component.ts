0001025d20298d21a44ea4fcd5e8d135644bfc5b
Podoshva <savushkinnikita503@gmail.com>
2023-04-18 21:45:20 +0300

    18

--- src/plugin/app.component.ts
+++ src/plugin/app.component.ts
@@ -4,50 +4,59 @@ import { environment } from 'src/environments/environment';
 import { UntypedFormControl } from '@angular/forms';
 import { city } from './city';
 
+interface AddressData {
+  Description: string;
+  MainDescription: string;
+  DeliveryCity: string;
+  Area: string;
+  Region: string;
+  SettlementType: string;
+  Ref: string;
+  DeliveryCityRegion: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-   
 })
 export class AppComponent implements OnInit {
   title = 'adr-search-ui-plugin';
-  adresURl=environment.NovaposhtaURL+'Address/searchSettlements/';
+  adresURl = environment.NovaposhtaURL + 'Address/searchSettlements/';
   nameControl!: UntypedFormControl;
   city: city | undefined;
-  cityes:city[]=[];
-  constructor( private http: HttpClient, ) { }
-    /** POST  from adreses the server */
+  cityes: city[] = [];
+  addresses: AddressData[] = [];
 
-ngOnInit() {
-  this.nameControl= new UntypedFormControl('Київ');
-  this.nameControl.valueChanges.subscribe((value) => console.log(value));
-  this.nameControl.statusChanges.subscribe((status) => console.log(status));
- // this.http.get('https://api.novaposhta.ua/v2.0/json/Address/searchSettlements/').subscribe((data:any) => this.citi=new Citi(data.Area, data.Present, data.Warehouses,data.MainDescription,data.Region,data.SettlementTypeCode,data.Ref,data.DeliveryCity,data.ParentRegionTypes,data.ParentRegionCode));
- this.getsearch()
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.nameControl = new UntypedFormControl('Київ');
+    this.nameControl.valueChanges.subscribe((value) => console.log(value));
+    this.nameControl.statusChanges.subscribe((status) => console.log(status));
+  }
+
+  search(): void {
+    const query = this.nameControl.value;
+    if (!query) {
+      return;
+    }
+
+    const postData = {
+      apiKey: '19d9c289108bc6ca5b3f9133f9185b89',
+      modelName: 'Address',
+      calledMethod: 'searchSettlements',
+      methodProperties: {
+        CityName:  "київ",
+        Limit: 50
+      }
+    };
+
+    this.http.post<any>(this.adresURl, postData).subscribe((data) => {
+      console.log(data);
+      this.city = data.data[0].Addresses[0];
+      this.cityes = data.data[0].Addresses;
+      this.addresses = data.data.map((item: any) => item.Addresses).flat();
+    });
+  }
 }
-
-
-
-getsearch(): any {
-  return this.http.post<any>(this.adresURl,{
-    "apiKey": "319e2177ec5d3aa8638e23b1e24fa487",
-     "modelName": "Address",
-        "calledMethod": "searchSettlements",
-        "methodProperties": {
-            "CityName": "київ",
-            "Limit": 5
-
-          }
-          
-      
-        
-    }).subscribe(x=>{console.log(x);
-    this.city=x.data[0].Addresses[0];
-    this.cityes=x.data[0].Addresses;
-    })
-}
-}
-
-
